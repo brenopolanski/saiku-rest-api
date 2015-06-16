0a632e246f33696717ee3126f10fd42a68e98f97
breno.polanski <breno.polanski@gmail.com>
2015-06-16 15:16:13 -0300

    Issue-18 #comment Add documentation YUIDoc

---
+++ api.js
@@ -0,0 +1,57 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ExportRequest",
+        "MDXRequest",
+        "RepositoryRequest",
+        "Saiku",
+        "SchemasRequest",
+        "SessionRequest"
+    ],
+    "modules": [
+        "ExportRequest",
+        "MDXRequest",
+        "RepositoryRequest",
+        "Saiku",
+        "SaikuSession",
+        "SchemasRequest",
+        "SessionRequest"
+    ],
+    "allModules": [
+        {
+            "displayName": "ExportRequest",
+            "name": "ExportRequest",
+            "description": "ExportRequest handle the /api/query/admin/export API endpoint"
+        },
+        {
+            "displayName": "MDXRequest",
+            "name": "MDXRequest",
+            "description": "MDXRequest handle the /api/query/execute API endpoint"
+        },
+        {
+            "displayName": "RepositoryRequest",
+            "name": "RepositoryRequest",
+            "description": "RepositoryRequest handle the /api/repository API endpoint"
+        },
+        {
+            "displayName": "Saiku",
+            "name": "Saiku",
+            "description": "A Saiku REST API client for Node.js"
+        },
+        {
+            "displayName": "SaikuSession",
+            "name": "SaikuSession"
+        },
+        {
+            "displayName": "SchemasRequest",
+            "name": "SchemasRequest",
+            "description": "SchemasRequest handle the /discover API endpoint"
+        },
+        {
+            "displayName": "SessionRequest",
+            "name": "SessionRequest",
+            "description": "SessionRequest handle the /session API endpoint"
+        }
+    ]
+} };
+});
